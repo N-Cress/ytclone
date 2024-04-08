d888dc2cfd6d16a6c97182a35c849a348cdcd840
Noah <azsnobird1227@yahoo.com>
2024-04-08 10:40:02 -0500

    header and pills, pills have some functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { PageHeader } from "./layouts/PageHeader"
+import { useState } from "react";
 
+import { PageHeader } from "./layouts/PageHeader";
+import { CatPills } from "./components/CatPills";
+
+import { categories } from "./data/home";
 
 function App() {
-
+  const [selectedCat, setSelectedCat] = useState(categories[0])
 
   return (
     <>
       <div className="max-h-screen flex flex-col">
         <PageHeader /> 
-        
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto" >
+          <div> Sidebar</div>
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4"> 
+              <CatPills categories={categories}
+              selectedCat={selectedCat} 
+              onSelect={setSelectedCat}/> 
+            </div>
+          </div>
+          
+        </div>
       </div>
     </>
   )
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,13 @@
-import { Menu, Upload, Bell, User, Mic } from "lucide-react"
+import { Menu, Upload, Bell, User, Mic, Search, ArrowLeft } from "lucide-react"
 import logo from "../assets/Logo.png"
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 export function PageHeader() {
+    const [showSearch, setShowSearch] = useState(false);
+
     return <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-       <div className=" flex gap-2 items-center flex-shrink-0">
+       <div className={`gap-4 items-center flex-shrink-0 ${showSearch ? "hidden" : "flex"}`}>
         <Button variant="ghost" size="icon" > 
         <Menu />
         </Button>
@@ -12,17 +15,28 @@ export function PageHeader() {
           <img src={logo} alt="" className="h-6"/>
         </a>
         </div>
-        <div>
-        <form className="flex gap-4 flex-grow justify-center" action="">
-            <div className="flex flex-grow">
-
+        <form className={`md:flex  gap-4 flex-grow justify-center ${showSearch ? "flex" : "hidden md:flex"}`}>
+            <Button onClick={() => setShowSearch(false)} className={`hover:bg-gray-300 flex-shrink-0 ${showSearch ? "flex" : "hidden"}`} size="icon" variant="ghost">
+                <ArrowLeft /> 
+            </Button>
+            <div className="flex flex-grow max-w-[600px] ">
+                <input type="search" placeholder="Search" className=" w-full pl-4 rounded-l-full border border-secondary-border shadow-inner shadow-secondary text-lg focus:border-blue-500 outline-none"/>
+                <Button className="rounded-r-full py-2 px-4 border-secondary-border border border-l-0 flex-shrink-0 hover:bg-gray-300">
+                    <Search /> 
+                </Button>
             </div>
-            <Button size="icon">
+            <Button className="hover:bg-gray-300" size="icon">
                 <Mic /> 
             </Button>
         </form>    
-        </div> 
-        <div className="flex flex-shrink-0 md:gap-2">
+
+        <div className={`flex flex-shrink-0 md:gap-2 ${showSearch ? "hidden" : "flex"}`}>
+            <Button onClick={() => setShowSearch(true)} size="icon" variant="ghost" className="md:hidden">
+                <Search />
+            </Button>
+            <Button size="icon" variant="ghost" className="md:hidden">
+                <Mic />
+            </Button>
             <Button size="icon" variant="ghost">
                 <Upload /> 
             </Button>
